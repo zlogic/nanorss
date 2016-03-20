fd0d422147dd1e586a9cda59d5d8903d8546f656
Dmitry Zolotukhin <zlogic@gmail.com>
2016-03-20 22:29:42 +0300

    Added persistence service. Added primitive configuration tool.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var i18n = require('i18n');
 var logger = require('./lib/services/logger');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var configuration = require('./routes/configuration');
 
 var app = express();
 
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower', express.static(path.join(__dirname , '/bower_components')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/configuration', configuration);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var i18n = require('i18n');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: i18n.__('nanoRSS') });
 });
 
 module.exports = router;
