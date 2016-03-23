5b82a59b9f7a59ae8b74dbe7669059b1be45de85
Dmitry Zolotukhin <zlogic@gmail.com>
2016-03-23 21:00:23 +0300

    Updated promise structure in index route. Added page origin name in index route (useful for RSS feeds). Removed unnecessary Bluebird promise imports. Bumped up sqlite3 dependency version.

--- routes/index.js
+++ routes/index.js
@@ -2,65 +2,72 @@ var express = require('express');
 var i18n = require('i18n');
 var persistence = require('../lib/services/persistence');
 var pagemonitorConfiguration = require('../lib/pagemonitor/configuration');
+var feedConfiguration = require('../lib/feed/configuration');
 var Promise = require('bluebird').Promise;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var processPagemonitorData = function(){
-    return persistence.getUserData().then(function(user){
-      return new Promise(function(resolve, reject){
-        pagemonitorConfiguration.configXml(user.pagemonitor, function(err, pagemonitorconfiguration){
-          if(err)
-            return reject(err);
-          var findPageMonitorConfiguration = function(url){
-            return pagemonitorconfiguration.pages.page.find(function(page){
-              return page.$.url === url;
-            });
-          };
-          persistence.getPageMonitorItems().then(function(monitoredPages){
-            var monitoredPagesItems = monitoredPages.map(function(page){
-              var pageMonitorConfiguration = findPageMonitorConfiguration(page.url);
-              var pageTitle = (pageMonitorConfiguration !== undefined )? pageMonitorConfiguration._ : undefined;
-              return {
-                date: page.updatedAt,
-                sortBy: new Date(page.updatedAt).getTime(),
-                title: pageTitle,
-                contents: page.delta.replace(/\n/g, '<br>\n'),
-                url: page.url
-              };
-            });
-            return resolve(monitoredPagesItems);
-          }).catch(reject);
-        });
+  persistence.getUserData().then(function(user){
+    var processPagemonitorData = new Promise(function(resolve, reject){
+      pagemonitorConfiguration.configXml(user.pagemonitor, function(err, pagemonitorconfiguration){
+        if(err)
+          return reject(err);
+        var findPageMonitorConfiguration = function(url){
+          return pagemonitorconfiguration.pages.page.find(function(page){
+            return page.$.url === url;
+          });
+        };
+        persistence.getPageMonitorItems().then(function(monitoredPages){
+          var monitoredPagesItems = monitoredPages.map(function(page){
+            var pageMonitorConfiguration = findPageMonitorConfiguration(page.url);
+            var pageTitle = (pageMonitorConfiguration !== undefined )? pageMonitorConfiguration._ : undefined;
+            return {
+              date: page.updatedAt,
+              sortBy: new Date(page.updatedAt).getTime(),
+              title: pageTitle,
+              origin: pageTitle,
+              contents: page.delta.replace(/\n/g, '<br>\n'),
+              url: page.url
+            };
+          });
+          resolve(monitoredPagesItems);
+        }).catch(reject);
       });
     });
-  };
-  var processFeedData = function(){
-    return persistence.getFeedItems().then(function(feedItems){
-      return feedItems.map(function(feedItem){
-        return {
-          date: feedItem.date,
-          sortBy: Math.min(new Date(feedItem.createdAt).getTime(), new Date(feedItem.date).getTime()),
-          title: feedItem.title,
-          contents: feedItem.contents,
-          url: feedItem.url
-        };
-      })
+    var processFeedData = new Promise(function(resolve, reject){
+      feedConfiguration.configXmlUrlNames(user.opml, function(err, feedNames){
+        if(err)
+          return reject(err);
+        persistence.getFeedItems().then(function(feedItems){
+          var feedItems = feedItems.map(function(feedItem){
+            var feedName = feedNames[feedItem.Feed.url];
+            if(feedName === undefined)
+              return undefined;
+            return {
+              date: feedItem.date,
+              sortBy: Math.min(new Date(feedItem.createdAt).getTime(), new Date(feedItem.date).getTime()),
+              title: feedItem.title,
+              origin: feedName,
+              contents: feedItem.contents,
+              url: feedItem.url
+            };
+          }).filter(function(feedItem){ return feedItem !== undefined;});
+          resolve(feedItems);
+        }).catch(reject);
+      });
     });
-  };
-  var items = [];
-  processPagemonitorData().then(function(newItems){
-    items = items.concat(newItems);
-    return processFeedData();
-  }).then(function(newItems){
-    items = items.concat(newItems);
-    items.sort(function(a, b){
-      return b.sortBy - a.sortBy;
-    })
-    res.render('index', {
-      title: i18n.__('nanoRSS'),
-      items: items
+    return Promise.all([processPagemonitorData, processFeedData]).then(function(items){
+      items = items.reduce(function(a, b){
+        return a.concat(b);
+      }, []);
+      items.sort(function(a, b){
+        return b.sortBy - a.sortBy;
+      });
+      res.render('index', {
+        title: i18n.__('nanoRSS'),
+        items: items
+      });
     });
   }).catch(next);
 });
