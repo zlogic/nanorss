ce91592e448cec291ee762b66e143ee2d5d9caee
Dmitry Zolotukhin <zlogic@gmail.com>
2016-03-24 11:24:01 +0300

    Fixed UI displaying duplicates of items when user opens/closes items too fast.

--- public/javascripts/loaditem.js
+++ public/javascripts/loaditem.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
   $('.collapse').on('shown.bs.collapse', function() {
     var placeholder = jQuery(this);
     $.get(placeholder.attr('data-fetchurl'), function(data) {
+      placeholder.empty();
       if(placeholder.attr('aria-expanded') === "true")
         placeholder.append(data);
-      else
-        placeholder.empty();
     });
   });
   $('.collapse').on('hidden.bs.collapse', function() {
