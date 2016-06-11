4f7e57533597682610c9aff87ce396c7e6209c81
Dmitry Zolotukhin <zlogic@gmail.com>
2016-06-11 20:09:47 +0300

    Moved worker into main Node Web app process. Bumped up dependency versions.

--- bin/worker.js
+++ bin/worker.js
@@ -1,29 +1,11 @@
 #!/usr/bin/env node
 
-var pagemonitor = require('../lib/pagemonitor/fetcher');
-var feed = require('../lib/feed/fetcher');
+var maintenanceworker = require('../lib/services/maintenanceworker');
 var persistence = require('../lib/services/persistence');
-var i18n = require('i18n');
-var Promise = require('bluebird').Promise;
-var logger = require('../lib/services/logger').logger;
 
 require('../lib/services/i18nconfiguration');
 require('../lib/services/needleconfiguration');
 
 persistence.init().then(function() {
-  var update = function(){
-    return Promise.all([pagemonitor.update(),feed.update()]).then(function(){
-      logger.info(i18n.__("Update completed"));
-    })
-  };
-  var cleanup = function(){
-    return Promise.all([pagemonitor.cleanup(),feed.cleanup()]).then(function(){
-      logger.info(i18n.__("Cleanup completed"));
-    })
-  };
-  var runTasks = function(){
-    return update().then(cleanup);
-  };
-  runTasks();
-  setInterval(runTasks, 15 * 60 * 1000);
+  maintenanceworker.startWorker();
 });
