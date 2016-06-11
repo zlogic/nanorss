0ed9aed50117a7852b613c9173db47e10788ecd3
Dmitry Zolotukhin <zlogic@gmail.com>
2016-06-12 02:55:02 +0300

    Fixed needle configuration being lost when moving the worker into main Node Web app process.

--- bin/worker.js
+++ bin/worker.js
@@ -4,7 +4,6 @@ var maintenanceworker = require('../lib/services/maintenanceworker');
 var persistence = require('../lib/services/persistence');
 
 require('../lib/services/i18nconfiguration');
-require('../lib/services/needleconfiguration');
 
 persistence.init().then(function() {
   maintenanceworker.startWorker();
