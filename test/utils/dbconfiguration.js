183f77cac0202abd80383e3fb8555ff7a990ca47
Dmitry Zolotukhin <zlogic@gmail.com>
2017-06-13 19:04:47 +0200

    Fixed Sequelize logger for tests.

--- test/utils/dbconfiguration.js
+++ test/utils/dbconfiguration.js
@@ -3,12 +3,12 @@ var persistence = require('../../lib/services/persistence');
 var path = require('path');
 var testdir = require('./testdir');
 var Sequelize = require('sequelize');
-var logger = require('../../lib/services/logger').logger;
+var logger = require('../../lib/services/logger');
 
 var reconfigureDb = function(inMemory){
   inMemory = inMemory !== undefined ? inMemory : true;
   var storage = inMemory === true ? ":memory:" : path.resolve(testdir.tmpdir, "nanoRSS.sqlite");
-  var currentPersistence = persistencefactory.model("sqlite:", {storage: storage, isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE, logging: logger.verbose});
+  var currentPersistence = persistencefactory.model("sqlite:", {storage: storage, isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE, logging: logger.sequelizeLogger});
   for(var k in currentPersistence)
     persistence[k] = currentPersistence[k];
 }
