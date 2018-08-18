41c456f023b42ae3e68a07a331c729a38e01a57b
Dmitry Zolotukhin <zlogic@gmail.com>
2018-08-18 23:20:25 +0200

    Enabled colors in logger.

--- test/utils/dbconfiguration.js
+++ test/utils/dbconfiguration.js
@@ -5,10 +5,8 @@ var testdir = require('./testdir');
 var Sequelize = require('sequelize');
 var logger = require('../../lib/services/logger');
 
-var reconfigureDb = function(inMemory){
-  inMemory = inMemory !== undefined ? inMemory : true;
-  var storage = inMemory === true ? ":memory:" : path.resolve(testdir.tmpdir, "nanoRSS.sqlite");
-  var currentPersistence = persistencefactory.model("sqlite:", {storage: storage, isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE, logging: logger.sequelizeLogger, operatorsAliases: false});
+var reconfigureDb = function(){
+  var currentPersistence = persistencefactory.model("sqlite:", {storage: ":memory:", isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE, logging: logger.sequelizeLogger, operatorsAliases: false});
   for(var k in currentPersistence)
     persistence[k] = currentPersistence[k];
 }
